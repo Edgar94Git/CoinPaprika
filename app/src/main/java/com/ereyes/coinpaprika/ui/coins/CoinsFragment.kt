98dc145063046c54dd5044222c66f428ebf8f942
Edgar Reyes <60406916+Edgar94Git@users.noreply.github.com>
2023-05-14 18:29:55 -0600

    is loaded progress bar

--- app/src/main/java/com/ereyes/coinpaprika/ui/coins/CoinsFragment.kt
+++ app/src/main/java/com/ereyes/coinpaprika/ui/coins/CoinsFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ereyes.coinpaprika.R
+import com.ereyes.coinpaprika.common.Constants
 import com.ereyes.coinpaprika.databinding.FragmentCoinsBinding
 import com.ereyes.coinpaprika.domain.model.Coin
 import com.ereyes.coinpaprika.ui.coinDetail.CoinDetailFragment
@@ -48,12 +50,15 @@ class CoinsFragment : Fragment() {
         viewModel.getMessages().observe(viewLifecycleOwner){ msg ->
             Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
         }
+        viewModel.isLoaded().observe(viewLifecycleOwner){ isLoaded ->
+            binding.pbLoadedCoins.isVisible = isLoaded
+        }
     }
 
     private fun onCoinSelected(coin: Coin){
 
         val arguments = Bundle()
-        arguments.putString("id", coin.Id)
+        arguments.putString(Constants.ID, coin.Id)
 
         val fragment = CoinDetailFragment()
         fragment.arguments = arguments
